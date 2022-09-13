b391d29d09faa8ccd41aa6df9f92c11c36b6d7e1
Leonardo <l.fvillar@bol.com.br>
2022-09-13 11:59:27 -0300

    Conversão de um array para lista e de lista para array usando foreach e for

--- Arrays e Matrizes/src/executavel/arrays/SplitArray.java
+++ Arrays e Matrizes/src/executavel/arrays/SplitArray.java
@@ -1,7 +1,12 @@
 package executavel.arrays;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class SplitArray {
 	public static void main(String[] args) {
+		
+		System.out.println("========== Percorrendo um array com For ==========");
 		
 		// O método split pega nossa variável texto e quebra ela em indices jogando dentro de um array
 		String text = "Leonardo,Curso Java,8,7,9,2";
@@ -11,5 +16,25 @@ public class SplitArray {
 		for(int pos = 0; pos < valoresArray.length; pos++) {
 			System.out.println("Valor na posição " + (pos + 1) + " é: " + valoresArray[pos]);
 		}
+		
+		// Convertendo um array em uma lista	
+		System.out.println("\n========== Convertendo array para uma lista e percorrendo com ForEach ==========");
+		
+		List<String> list = Arrays.asList(valoresArray);
+		
+		for (String valorString : list) {
+			System.out.println(valorString);
+		}
+		
+		//Convertendo uma lista para array
+		System.out.println("\n========== Convertendo lista para um array e percorrendo com For ==========");
+
+		String[] conversaoArray = list.toArray(new String[6]);
+		
+		for(int pos = 0; pos < conversaoArray.length; pos++) {
+			System.out.println("Valor na posição " + (pos + 1) + " é: " + conversaoArray[pos]);
+		}
+		
+		
 	}
 }
